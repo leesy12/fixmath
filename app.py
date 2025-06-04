5d1e8cdb7cab7e999d1d644c9c035ae949fa6382
leeeeegayeon <kathy5636@naver.com>
2025-06-05 03:58:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 # 서버 시작 (Render에서는 필요 없음, 로컬 디버깅용)
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
